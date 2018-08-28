f23574d239c67b0e5ffdaf096a68106e0bad7424
Andrii Konchak <>
2018-08-28 12:28:03 +0300

    DetailViewController

--- MoviesApp/Controller/DetailViewController.swift
+++ MoviesApp/Controller/DetailViewController.swift
@@ -17,14 +17,17 @@ class DetailViewController: UIViewController {
     var apiMovies = MoviesApi()
     var mainView = MainCollectionViewController()
     var movieModelDetails: DiscoveryResponse.DiscoveryMovieModel?
+
     private var response: MovieVideoModel?
     private var movieTrailers: MovieVideoModel.Results?
     
     @IBOutlet weak var fullImage: UIImageView!
     @IBOutlet weak var moviesTitle: UILabel!
     @IBOutlet weak var movieSummary: UILabel!
-    @IBOutlet weak var releaseDate: UILabel!
     @IBOutlet weak var videoWebView: WKWebView!
+    @IBOutlet weak var movieReleaseDate: UILabel!
+    @IBOutlet weak var movieRating: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +41,7 @@ class DetailViewController: UIViewController {
             self.getVideo(videoCode: response?.results.first?.key ?? "")
         }
         downloadElements()
+        releaseDateFormatter()
     }
     
     func getVideo(videoCode: String) {
@@ -47,14 +51,37 @@ class DetailViewController: UIViewController {
     
     func downloadElements() {
         
-            self.moviesTitle.text = self.movieModelDetails?.title
-            self.movieSummary.text = self.movieModelDetails?.overview
-            self.releaseDate.text = self.movieModelDetails?.release_date
-            
-            let backdropURL = "https://image.tmdb.org/t/p/w500"
+        self.moviesTitle.text = self.movieModelDetails?.title
+        self.movieRating.text = forTrailingZero(temp: self.movieModelDetails?.vote_average ?? 0.0) 
+        self.movieSummary.text = self.movieModelDetails?.overview
+       
+        let backdropURL = "https://image.tmdb.org/t/p/w500"
             if let backdropModel = self.movieModelDetails?.backdrop_path {
                 let resource = ImageResource(downloadURL: URL(string: backdropURL + backdropModel)!, cacheKey: backdropURL + backdropModel)
                 self.fullImage.kf.setImage(with: resource)
             }
         }
+    
+    // Rounding Vote Rating
+    func forTrailingZero(temp: Double) -> String {
+        let tempVar = String(format: "%g", temp)
+        return tempVar
     }
+    
+    // Release date formatter
+    func releaseDateFormatter() {
+        
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd"
+        
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "MMM dd,yyyy"
+        
+        if let date = dateFormatterGet.date(from: movieModelDetails?.release_date ?? "" ){
+            self.movieReleaseDate.text = dateFormatterPrint.string(from: date)
+        }
+        else {
+            print("There was an error decoding the string")
+        }
+    }
+}
